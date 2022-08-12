900385a3e011b763b6cac9940725b7c8797d8cda
Victor Matias <fvictor1705@gmail.com>
2022-08-12 12:53:59 -0300

    Refatora função filterProducts.

--- src/services/services.js
+++ src/services/services.js
@@ -1,14 +1,12 @@
 export function filterProducts(param) {
-  const filteredProducts = [];
-
-  param.reduce((acc, curr) => {
-    if (!acc.includes(curr.title)) {
-      acc.push(curr.title);
-      return acc;
-    } return acc;
-  }, []).forEach((title) => {
-    filteredProducts.push(param.find((el) => el.title === title));
-  }); return filteredProducts;
+  return param
+    .reduce((acc, curr) => {
+      if (!acc.includes(curr.title)) {
+        acc.push(curr.title);
+        return acc;
+      } return acc;
+    }, [])
+    .map((title) => param.find((el) => el.title === title));
 }
 
 export const updateCounter = (param) => param.setState((pS) => ({ count: pS.count + 1 }));
